761ae47d13b5a5bc19863dabe6ecc0e5bfec8601
Josue Basurto <josuebasurto@gmail.com>
2023-07-12 23:08:00 -0700

    Y se logro!

--- popup.js
+++ popup.js
@@ -1,19 +1,47 @@
-alert('Hacker News Opener!');
+const seconds = 1;
 
-fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
+document.addEventListener('DOMContentLoaded', function () {
+  const newsList = document.getElementById('newsList');
+  const openBtn = document.getElementById('openBtn');
+
+  // Obtener los 5 links más recientes de Hacker News
+  fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
     .then(response => response.json())
     .then(data => {
-      var topStories = data.slice(0, 10);
+      const topStories = data.slice(0, 10);
 
       topStories.forEach(storyId => {
         fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`)
           .then(response => response.json())
           .then(story => {
-            setTimeout(()=>{},5000);
-            chrome.tabs.create({ url: story.url });
+            const listItem = document.createElement('li');
+            const link = document.createElement('a');
+            link.href = story.url;
+            link.target = '_blank';
+            link.textContent = story.title + ' - ' + story.text;
+            listItem.appendChild(link);
+            newsList.appendChild(listItem);
           });
-        
-          chrome.tabs.create({ url: 'https://news.ycombinator.com/' });
       });
     });
 
+  // Abrir los links en nuevas pestañas en segundo plano de inmediato
+  openBtn.addEventListener('click', function () {
+    const links = Array.from(newsList.getElementsByTagName('a'));
+    const numLinks = links.length;
+
+    const confirmation = confirm(`¿Deseas abrir ${numLinks} páginas en segundo plano?`);
+
+    if (confirmation) {
+      openLinksInBackground(links);
+    }
+  });
+
+  // Función para abrir los links en nuevas pestañas en segundo plano de inmediato
+  function openLinksInBackground(links) {
+    links.forEach((link, index) => {
+      chrome.tabs.create({ url: link.href, active: false });
+    });
+  }
+
+});
