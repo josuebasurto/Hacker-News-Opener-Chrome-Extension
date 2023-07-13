30a85a614123d69ac27c45c4d4d90a11a959b2e2
Josue Basurto <josuebasurto@gmail.com>
2023-07-12 22:44:29 -0700

    ammend commit

--- popup.js
+++ popup.js
@@ -1,7 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var openBtn = document.getElementById('openBtn');
-    openBtn.addEventListener('click', function() {
-      chrome.runtime.sendMessage({ action: 'openHackerNews' });
+alert('Hacker News Opener!');
+
+fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
+    .then(response => response.json())
+    .then(data => {
+      var topStories = data.slice(0, 10);
+
+      topStories.forEach(storyId => {
+        fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`)
+          .then(response => response.json())
+          .then(story => {
+            setTimeout(()=>{},5000);
+            chrome.tabs.create({ url: story.url });
+          });
+        
+          chrome.tabs.create({ url: 'https://news.ycombinator.com/' });
+      });
     });
-  });
-  
+
